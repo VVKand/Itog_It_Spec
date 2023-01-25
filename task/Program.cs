ae01b74100135f4ffc7674c43c4d83135f7dd588
vkand <vvkand@mail.ru>
2023-01-25 22:14:21 +0700

    написал и проверил программу

---
+++ task/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] mas = { "Hello", "2", "world", ":-)", "1234", "1567", "-2" };
+Sort(mas);
+
+void Sort(string[] array)
+{
+    for (int i = 0; i < mas.Length; i++)
+        if (mas[i].Length <= 3)
+            Console.Write(mas[i] + ", ");
+}
